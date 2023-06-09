ec73ebf861a1312d83d029f61eb6fafef6c9f195
Daniel-DaMata <daniel.soares.damata@gmail.com>
2023-06-09 17:19:01 -0300

    add HospitalEspecialidadeTest

--- QuantoDemoraApi.Tests/RepositoryTests/HospitalEspecialidadesRepositoryTest.cs
+++ QuantoDemoraApi.Tests/RepositoryTests/HospitalEspecialidadesRepositoryTest.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuantoDemoraApi.Models;
 
 namespace QuantoDemoraApi.Tests.RepositoryTests
 {
@@ -17,7 +18,7 @@ namespace QuantoDemoraApi.Tests.RepositoryTests
         private readonly DataContext _context;
         private readonly Mock<IHospitalEspecialidadesRepository> _mockRepository;
         private HospitalEspecialidadesRepository _repository;
-
+        //private int hospitalId =1;
 
         public HospitalEspecialidadesRepositoryTest()
         {
@@ -30,6 +31,45 @@ namespace QuantoDemoraApi.Tests.RepositoryTests
             _repository = new HospitalEspecialidadesRepository(_context);
         }
 
+        /* [Fact]
+         public async void GetAllAsync_RetornarListaHospitalEspecialidade()
+         {
+             // Arrange
+             var tc = new List<Hospital>
+             { 
+             new Hospital { IdHospital = 2, HospitalEspecialidades = Especialidade.IdEspecialidade },
+             new Hospital { IdHospital = 3, IdEspecialidade = 1 }
+             };
+             _mockRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(tc);
 
+             //Act
+             var result = await _repository.GetAllAsync();
+
+             //Assert
+             Assert.IsType<List<Hospital>>(result);
+         }*/
+        [Fact]
+        public async void GetByIdAsync_RetornarLogradouroPeloIdInformado()
+        {
+            //Arrange
+            using var context = new DataContext(_options);
+            var repo = new HospitalEspecialidadesRepository(context);
+
+            var tc = new HospitalEspecialidade
+            {
+                IdEspecialidade = 1,
+                IdHospital = 1
+            };
+            context.HospitalEspecialidades.Add(tc);
+            await context.SaveChangesAsync();
+
+            //Act
+            var result = await repo.GetByIdAsync(tc.IdHospital);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(tc.IdHospital, result.IdHospital);
+           // Assert.Equal(tc.IdEspecialidade, result.IdEspecialidade);
+        }
     }
 }
